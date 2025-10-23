9ab4041a72957cec5b174c19eb79bbcea38a4bc4
Samatha Pulgam <pulgamsamatha@gmail.com>
2025-10-23 14:20:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegistrationForm from "./Components/RegistrationForm";
+
+const App = () => {
+  return (
+    <div>
+      <RegistrationForm/>
+    </div>
+  );
+};
+
+export default App;
